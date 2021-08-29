3b151cd5152d6a907b5390edd33c47231b595f88
SERGIO ALEXANDER SOSA RODRIGUEZ <sergiososa108083@correo.itm.edu.co>
2021-08-28 23:25:51 -0500

    Project structure, folder Models and Responses

---
+++ time.Common/Models/Time.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace time.Common.Models
+{
+    public class Time
+    {
+        public int EmployeId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int Type { get; set; }
+
+        public bool IsConsolidated { get; set; }
+    }
+}
